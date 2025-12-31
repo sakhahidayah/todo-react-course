551bce9b572c81453ce66d38a3cc287b2f70f22c
sakhahidayah <sakhahidayahramadhan@gmail.com>
2025-12-31 12:55:43 +0700

    Add layout for todo form

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,23 +1,37 @@
+import styles from "./TodoForm.module.css";
+
 export function TodoForm() {
   return (
     <section>
-      <h3>New To-Do</h3>
+      <h3 className={styles.Title}>New To-Do</h3>
 
-      <form>
-        <input type="text" aria-label="Name*" placeholder="Name*" name="name" autoComplete="off" />
+      <form className={styles.Form}>
+        <div className={styles.FormFields}>
+          <div className={styles.FormField}>
+            <input type="text" aria-label="Name*" placeholder="Name*" name="name" autoComplete="off" />
+          </div>
 
-        <textarea aria-label="Description" placeholder="Description" name="description" rows="3" />
+          <div className={styles.FormField}>
+            <textarea aria-label="Description" placeholder="Description" name="description" rows="3" />
+          </div>
 
-        <label htmlFor="deadline">Deadline</label>
-        <input type="date" id="deadline" name="deadline" />
+          <div className={styles.FormGroup}>
+            <div className={styles.FormField}>
+              <label htmlFor="deadline">Deadline</label>
+              <input type="date" id="deadline" name="deadline" />
+            </div>
 
-        <label htmlFor="priority">Priority</label>
-        <select defaultValue="none" id="priority" name="priority">
-          <option value="none">None</option>
-          <option value="low">Low</option>
-          <option value="medium">Medium</option>
-          <option value="high">High</option>
-        </select>
+            <div className={styles.FormField}>
+              <label htmlFor="priority">Priority</label>
+              <select defaultValue="none" id="priority" name="priority">
+                <option value="none">None</option>
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+              </select>
+            </div>
+          </div>
+        </div>
 
         <input type="submit" value="Add" />
       </form>
